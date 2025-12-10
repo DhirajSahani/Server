3827f64c4d485a418dc03102be9a2bce9ebea430
Dhiraj Sahani <72699288+DhirajSahani@users.noreply.github.com>
2025-12-10 13:44:40 +0545

    fix db.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -18,10 +17,8 @@ app.get("/", (req, res) => {
 
 app.use("/book", bookrouter);
 
-const API = "https://your-render-backend-url.onrender.com"; // ✅
+const PORT = process.env.PORT || 5000;
 
-
-// VERY IMPORTANT for Render
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
