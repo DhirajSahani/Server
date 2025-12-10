58b953c7bf03609aad9618d3e218576c837d735f
Dhiraj Sahani <72699288+DhirajSahani@users.noreply.github.com>
2025-12-10 13:20:21 +0545

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,24 +1,13 @@
-// config/db.js
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
   }
-  cached.conn = await cached.promise;
-  console.log("MongoDB Connected");
-  return cached.conn;
 };
 
 export default connectDB;
